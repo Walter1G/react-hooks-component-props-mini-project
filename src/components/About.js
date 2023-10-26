06b069e9bfa99a8a1f18897269687b6653032722
Walter N <2760walter@gmail.com>
2023-10-26 08:35:23 +0300

    Add about and header

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import react from "react";
+
+function About({ src = "https://via.placeholder.com/215", about }) {
+
+    return (
+        <aside>
+            <img src={src} alt="blog logo" />
+            <p>{about}</p>
+        </aside>
+    );
+
+}
+
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import react from "react";
+
+function Header({ name }) {
+
+    console.log({ name })
+
+
+    return (
+        <header>
+            <h1>{name}</h1>
+        </header>
+
+    );
+}
+
+export default Header;
